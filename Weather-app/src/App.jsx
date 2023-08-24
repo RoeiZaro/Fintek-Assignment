45e8e61ffa177e8a7527c345ec194d50a9aa41a4
RoeiZaro <Roeip21@gmail.com>
2023-08-24 12:07:43 +0300

    wrapping in a div and giving attribute

--- Weather-app/src/App.jsx
+++ Weather-app/src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div id='app-conteiner'>
       <WeatherForm />
       <WeatherPopup />
-    </>
+    </div>
   )
 }
 
