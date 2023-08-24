92f90cc69b9eea34c4145d78948f9fa5f7f18c90
RoeiZaro <Roeip21@gmail.com>
2023-08-24 13:05:33 +0300

    adding functionilly

--- Weather-app/src/Components/WeatherForm.jsx
+++ Weather-app/src/Components/WeatherForm.jsx
@@ -7,19 +7,22 @@ const text = `
     to see the weather
     around the world
   `
-// form handling
+function WeatherForm({cityName, setCityName}) {
+
+  // form handling
   const handleChange = (eve) =>{
     setCityName(eve.target.value)
-}
+  }
 
-const handleSubmit = (eve) => {
-  eve.preventDefault()
-  //send API request and  w8 for response, then show to weather
-  console.log(cityName);
-}
+  const handleSubmit = (eve) => {
+    eve.preventDefault()
+    //send API request and  w8 for response, then show to weather
+    console.log(cityName);
+  }
 
-function WeatherForm() {
-  return (
+
+
+    return (
     <div id='form-conteiner'>
         {/* opening massage */}
        <pre>{text}</pre>
@@ -31,7 +34,7 @@ function WeatherForm() {
 
         {/* the input and the submit area */}
         <div id="inputConteiner">
-          <input id='cityNameInput' type='text' name='cityName' value={"cityName"} onChange={(eve)=>handleChange(eve)} />
+          <input id='cityNameInput' type='text' name='cityName' value={cityName} onChange={(eve)=>handleChange(eve)} />
           <input id='submit' type='submit' value="Check"/>
         </div>
           
