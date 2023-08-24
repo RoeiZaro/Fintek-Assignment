464306e82817f626eea21a9ed2bd4286e7202077
RoeiZaro <Roeip21@gmail.com>
2023-08-24 11:43:52 +0300

    baseline elements with dummy data

--- Weather-app/src/Components/WeatherPopup.jsx
+++ Weather-app/src/Components/WeatherPopup.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 function WeatherPopup() {
   return (
-    <div>WeatherPopup</div>
+    <div>
+         <div id='popup'>
+          <div id='location-section'>
+            <p>Tel Aviv</p>
+            <p>Israel</p>
+          </div>
+
+          <div id='temperature-section'>
+            <p>18*</p>
+          </div>
+
+          <div id='bottom-info'>
+            <div>
+              <p>Precipitation</p>
+              <p>0 mm</p>
+            </div>
+            <div>
+              <p>Humidity</p>
+              <p>53%</p>
+            </div>
+            <div>
+              <p>Wind</p>
+              <p>23 km/h</p>
+            </div>
+        </div>
+
+        </div>
+
+    </div>
   )
 }
 
