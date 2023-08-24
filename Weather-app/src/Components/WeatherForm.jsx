95c5842cac74fea35906f794ae89784922de254f
RoeiZaro <Roeip21@gmail.com>
2023-08-24 11:41:59 +0300

    baseline of the component

--- Weather-app/src/Components/WeatherForm.jsx
+++ Weather-app/src/Components/WeatherForm.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
+const text = `
+    Use our weather app
+    to see the weather
+    around the world
+  `
+  const handleChange = (eve) =>{
+    setCityName(eve.target.value)
+}
+
+const handleSubmit = (eve) => {
+  eve.preventDefault()
+  //send API request and  w8 for response, then show to weather
+  console.log(cityName);
+}
 
 function WeatherForm() {
   return (
-    <div>WeatherForm</div>
+    <div>
+       <pre>{text}</pre>
+      
+       <form onSubmit={(eve)=>handleSubmit(eve)}>
+
+        <label value=''>City Name:</label>
+
+        <div id="inputConteiner">
+          <input id='cityNameInput' type='text' name='cityName' value={"cityName"} onChange={(eve)=>handleChange(eve)} />
+          <input id='submit' type='submit' value="Check"/>
+        </div>
+          
+      </form>
+    </div>
   )
 }
 
