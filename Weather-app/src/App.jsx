9a9bc9770cab20267686d3ad2def4ef37e67f868
RoeiZaro <Roeip21@gmail.com>
2023-08-25 11:00:27 +0300

    adding comments

--- Weather-app/src/App.jsx
+++ Weather-app/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import WeatherForm from './Components/WeatherForm'
-import WeatherPopup from './Components/WeatherPopup'
-import './App.css'
+import { useState } from "react";
+import WeatherForm from "./Components/WeatherForm";
+import WeatherPopup from "./Components/WeatherPopup";
+import "./App.css";
 
 function App() {
-  const [weatherData, setWeatherData] = useState("")
-  
+  // weather information state
+  const [weatherData, setWeatherData] = useState("");
+
   return (
-    <div id='app-conteiner'>
-      <WeatherForm setWeatherData={setWeatherData}/>
-      <WeatherPopup  weatherData={weatherData}/>
+    <div id="app-conteiner">
+      {/* text+input+submit botton */}
+      <WeatherForm setWeatherData={setWeatherData} />
+
+      {/* the weather popup that appear after getting the weather data */}
+      <WeatherPopup weatherData={weatherData} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
